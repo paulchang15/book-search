975e1e694039a60d0b449fa0284931cccdff9176
Paul Chang <paulchang959@gmail.com>
2021-04-16 16:59:38 -0700

    initial commit

---
+++ server/Schemas/resolvers.js
@@ -0,0 +1,11 @@
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    user: async (parent, { username }) => {
+      return User.findOne({ username });
+    },
+  },
+};
